832cf85a094f93800a667b0c8cbf9ac6580f02f0
홍영기 <siesto@webworker.local>
2020-03-20 13:56:06 +0900

    Updates

---
+++ precache-manifest.b084ba5424dbbb7c5c93a54a6a7521a8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9058bf4c4ab8c7e06f264383baa67810",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bf70e0bc1cd05371f4cd",
+    "url": "/static/css/main.f4c90035.chunk.css"
+  },
+  {
+    "revision": "8fa10abb204d7593cce7",
+    "url": "/static/js/2.a620ccf4.chunk.js"
+  },
+  {
+    "revision": "2d4e50db9bebc1822939159016933eb5",
+    "url": "/static/js/2.a620ccf4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bf70e0bc1cd05371f4cd",
+    "url": "/static/js/main.861f8140.chunk.js"
+  },
+  {
+    "revision": "1e13aa549805b89c797f",
+    "url": "/static/js/runtime-main.7759213a.js"
+  }
+]);
